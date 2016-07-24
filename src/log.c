ae0a146c55ab830a69718d4aa7c73a60dfcf284b
sjs205 <swannonline@googlemail.com>
2016-07-24 23:49:10 +0100

    Update loggin functions to use a va_list version

--- src/log.c
+++ src/log.c
@@ -4,7 +4,7 @@
  * Author: Steven Swann - swannonline@googlemail.com
  *
  * Copyright (c) swannonline, 2013-2014
- * 
+ *
  * This file is part of sensorspace.
  *
  * sensorspace is free software: you can redistribute it and/or modify
@@ -23,7 +23,7 @@
  *****************************************************************************/
 #include "log.h"
 
-/* 
+/*
  * \brief function to convert log_level to user readable string
  * \param level the log level of the message to be logged
  * \param buf The log level string
@@ -55,7 +55,7 @@ static void get_log_level_str(log_level_t level, char *buf) {
       break;
   }
 
-  return; 
+  return;
 }
 
 /*
@@ -66,7 +66,7 @@ static void get_log_level_str(log_level_t level, char *buf) {
  */
 log_level_t set_log_level_str(char *level) {
 
- log_level_t ret; 
+ log_level_t ret;
   if (!strcmp(level, "QUIET")) {
     ret = log_level(LOG_QUIET);
   } else if(!strcmp(level, "INFO")) {
@@ -94,7 +94,7 @@ log_level_t set_log_level_str(char *level) {
 
 /*
  * \brief function to set/get log level
- * \param level When true the log level is set, 
+ * \param level When true the log level is set,
  * \return the current log level
  */
 log_level_t log_level(log_level_t level) {
@@ -109,17 +109,12 @@ log_level_t log_level(log_level_t level) {
 }
 
 /*
- * \brief function to print log messages to stdout
+ * \brief function to print log messages in va_args format to stdout
  * \param level the log level of the message to be logged
  * \param format String to be logged
- * \param ... additional arguments for format
+ * \param args additional arguments for format
  */
-void log_stdout(log_level_t level, const char *format, ...) {
-
-  va_list args;
-
-  va_start(args, format);
-
+void log_stdout_args(log_level_t level, const char *format, va_list args) {
   if (log_level(0) >= level) {
 
     if (level > LOG_INFO) {
@@ -134,22 +129,16 @@ void log_stdout(log_level_t level, const char *format, ...) {
     fprintf(stdout, "\n");
   }
 
-  va_end(args);
-
+  return;
 }
 
 /*
- * \brief function to print log messages to stderr
+ * \brief function to print log messages in va_args format to stderr
  * \param level the log level of the message to be logged
  * \param format String to be logged
- * \param ... additional arguments for format
+ * \param args additional arguments for format
  */
-void log_stderr(log_level_t level, const char *format, ...) {
-
-  va_list args;
-
-  va_start(args, format);
-
+void log_stderr_args(log_level_t level, const char *format, va_list args) {
   if (log_level(0) >= level) {
 
     /* Automatically print msg type for std_err */
@@ -164,8 +153,45 @@ void log_stderr(log_level_t level, const char *format, ...) {
     fprintf(stderr, "\n");
   }
 
+  return;
+}
+
+/*
+ * \brief function to print log messages to stdout
+ * \param level the log level of the message to be logged
+ * \param format String to be logged
+ * \param ... additional arguments for format
+ */
+void log_stdout(log_level_t level, const char *format, ...) {
+
+  va_list args;
+
+  va_start(args, format);
+
+  log_stdout_args(level, format, args);
+
   va_end(args);
 
+  return;
+}
+
+/*
+ * \brief function to print log messages to stderr
+ * \param level the log level of the message to be logged
+ * \param format String to be logged
+ * \param ... additional arguments for format
+ */
+void log_stderr(log_level_t level, const char *format, ...) {
+
+  va_list args;
+
+  va_start(args, format);
+
+  log_stderr_args(level, format, args);
+
+  va_end(args);
+
+  return;
 }
 
 /*
