sjs205/uMQTT

commit 7cad0bb3d965f0ef783ed9acceaa68728f5d6356

Add macros that enable debugging to be removed

src/apps/uMQTT_pub.c
@@ -95,7 +95,7 @@ umqtt_ret file_read_contents(const char *filename, uint8_t *buf, size_t *len) {
   fseek(f, 0, SEEK_SET);
 
   if (fsize > *len) {
-    log_std(LOG_ERROR, "The file (%zu bytes) is larger than buffer (%zu bytes)",
+    LOG_ERROR("The file (%zu bytes) is larger than buffer (%zu bytes)",
       fsize, *len);
     fclose(f);
     ret = UMQTT_PAYLOAD_ERROR;
@@ -160,7 +160,7 @@ int main(int argc, char **argv) {
           if (optarg) {
             strcpy(topic, optarg);
           } else {
-            log_std(LOG_ERROR, "The topic flag should be followed by a topic");
+            LOG_ERROR("The topic flag should be followed by a topic");
             return print_usage();
           }
           break;
@@ -170,7 +170,7 @@ int main(int argc, char **argv) {
           if (optarg) {
             strcpy(msg, optarg);
           } else {
-            log_std(LOG_ERROR, "The message flag should be followed by a message");
+            LOG_ERROR("The message flag should be followed by a message");
             return print_usage();
           }
           break;
@@ -180,7 +180,7 @@ int main(int argc, char **argv) {
           if (optarg) {
             strcpy(filename, optarg);
           } else {
-            log_std(LOG_ERROR, "The file flag should be followed by a file");
+            LOG_ERROR("The file flag should be followed by a file");
             return print_usage();
           }
           break;
@@ -190,7 +190,7 @@ int main(int argc, char **argv) {
           if (optarg) {
             strcpy(broker_ip, optarg);
           } else {
-            log_std(LOG_ERROR, "The broker flag should be followed by an IP address");
+            LOG_ERROR("The broker flag should be followed by an IP address");
             return print_usage();
           }
           break;
@@ -200,7 +200,7 @@ int main(int argc, char **argv) {
           if (optarg) {
             broker_port = *optarg;
           } else {
-            log_std(LOG_ERROR, "The port flag should be followed by a port");
+            LOG_ERROR("The port flag should be followed by a port");
             return print_usage();
           }
           break;
@@ -210,8 +210,7 @@ int main(int argc, char **argv) {
           if (optarg) {
             strcpy(clientid, optarg);
           } else {
-            log_std(LOG_ERROR,
-                "The clientid flag should be followed by a clientid");
+            LOG_ERROR("The clientid flag should be followed by a clientid");
             return print_usage();
           }
           break;
@@ -224,17 +223,17 @@ int main(int argc, char **argv) {
   }
 
   if (msg[0] && filename[0]) {
-      log_std(LOG_ERROR, "The PUBLISH message is missing");
+      LOG_ERROR("The PUBLISH message is missing");
       return -1;
   }
 
   struct broker_conn *conn;
 
-  log_std(LOG_INFO, "Initialisig socket connection");
+  LOG_INFO("Initialisig socket connection");
 
   init_linux_socket_connection(&conn, broker_ip, sizeof(broker_ip), broker_port);
   if (!conn) {
-    log_std(LOG_INFO, "XError: Initialising socket connection");
+    LOG_INFO("XError: Initialising socket connection");
     return -1;
   }
 
@@ -242,32 +241,32 @@ int main(int argc, char **argv) {
     broker_set_clientid(conn, clientid, sizeof(clientid));
   }
 
-  log_std(LOG_INFO, "Connecting to broker");
+  LOG_INFO("Connecting to broker");
 
   struct linux_broker_socket *skt = '\0';
   if ((ret = broker_connect(conn))) {
-    log_std(LOG_ERROR, "Initialising socket connection");
+    LOG_ERROR("Initialising socket connection");
     free_connection(conn);
     return ret;
   } else {
     skt = (struct linux_broker_socket *)conn->context;
-    log_std(LOG_INFO, "Connected to broker:\nip: %s port: %d", skt->ip, skt->port);
+    LOG_INFO("Connected to broker:\nip: %s port: %d", skt->ip, skt->port);
   }
 
-  log_std(LOG_INFO, "Constructing MQTT PUBLISH packet with:");
-  log_std(LOG_INFO, "Topic: %s", topic);
-  log_std(LOG_INFO, "Message: %s", msg);
+  LOG_INFO("Constructing MQTT PUBLISH packet with:");
+  LOG_INFO("Topic: %s", topic);
+  LOG_INFO("Message: %s", msg);
 
   struct mqtt_packet *pkt = construct_packet_headers(PUBLISH);
 
   if (!pkt || (ret = set_publish_variable_header(pkt, topic, strlen(topic)))) {
-    log_std(LOG_ERROR, "Setting up packet");
+    LOG_ERROR("Setting up packet");
     ret = UMQTT_ERROR;
     goto free;
   }
 
   if ((ret = set_publish_fixed_flags(pkt, retain, 0, 0))) {
-    log_std(LOG_ERROR, "Setting publish flags");
+    LOG_ERROR("Setting publish flags");
     ret = UMQTT_ERROR;
     goto free;
   }
@@ -278,20 +277,20 @@ int main(int argc, char **argv) {
     size_t len = MAX_REMAIN_LEN_PRODUCT;
     uint8_t *buf = calloc(sizeof(uint8_t), len);
     if (!buf) {
-      log_std(LOG_ERROR, "File buffer allocation failed");
+      LOG_ERROR("File buffer allocation failed");
       ret = UMQTT_ERROR;
       goto free;
     }
 
     if ((ret = file_read_contents(filename, buf, &len))) {
-      log_std(LOG_ERROR, "Reading file failed");
+      LOG_ERROR("Reading file failed");
       ret = UMQTT_ERROR;
       free (buf);
       goto free;
     }
 
     if ((ret = init_packet_payload(pkt, PUBLISH, buf, len))) {
-      log_std(LOG_ERROR, "Attaching payload");
+      LOG_ERROR("Attaching payload");
       ret = UMQTT_ERROR;
       free (buf);
       goto free;
@@ -302,7 +301,7 @@ int main(int argc, char **argv) {
   } else {
 
     if ((ret = init_packet_payload(pkt, PUBLISH, (uint8_t *)msg, strlen(msg)))) {
-      log_std(LOG_ERROR, "Attaching payload");
+      LOG_ERROR("Attaching payload");
       ret = UMQTT_ERROR;
       goto free;
     }
@@ -310,17 +309,20 @@ int main(int argc, char **argv) {
 
   finalise_packet(pkt);
 
-  log_std(LOG_INFO, "Sending packet to broker");
+  LOG_INFO("Sending packet to broker");
 
+  int i =0;
+  while (i++ < 0xff) {
   if ((ret = broker_send_packet(conn, pkt))) {
-    log_std(LOG_ERROR, "Sending packet failed");
+    LOG_ERROR("Sending packet failed");
 
   } else {
-    log_std(LOG_INFO, "Successfully sent packet");
+    LOG_INFO("Successfully sent packet");
+  }
   }
 
 free:
-  log_std(LOG_INFO, "Disconnecting from broker");
+  LOG_INFO("Disconnecting from broker");
   broker_disconnect(conn);
   free_connection(conn);
   free_packet(pkt);

src/apps/uMQTT_sub.c
@@ -130,7 +130,7 @@ int main(int argc, char **argv) {
           if (optarg) {
             strcpy(topic, optarg);
           } else {
-            log_std(LOG_ERROR, "The topic flag should be followed by a topic.");
+            LOG_ERROR("The topic flag should be followed by a topic.");
             return print_usage();
           }
           break;
@@ -140,7 +140,7 @@ int main(int argc, char **argv) {
           if (optarg) {
             strcpy(broker_ip, optarg);
           } else {
-            log_std(LOG_ERROR, "The broker flag should be followed by an IP address.");
+            LOG_ERROR("The broker flag should be followed by an IP address.");
             return print_usage();
           }
           break;
@@ -150,7 +150,7 @@ int main(int argc, char **argv) {
           if (optarg) {
             broker_port = *optarg;
           } else {
-            log_std(LOG_ERROR, "The port flag should be followed by a port.");
+            LOG_ERROR("The port flag should be followed by a port.");
             return print_usage();
           }
           break;
@@ -160,8 +160,7 @@ int main(int argc, char **argv) {
           if (optarg) {
             strcpy(clientid, optarg);
           } else {
-            log_std(LOG_ERROR,
-                "The clientid flag should be followed by a clientid");
+            LOG_ERROR("The clientid flag should be followed by a clientid");
             return print_usage();
           }
           break;
@@ -173,11 +172,11 @@ int main(int argc, char **argv) {
 
   struct broker_conn *conn;
 
-  log_std(LOG_INFO, "Initialising socket connection");
+  LOG_INFO("Initialising socket connection");
 
   init_linux_socket_connection(&conn, broker_ip, sizeof(broker_ip), broker_port);
   if (!conn) {
-    log_std(LOG_ERROR, "Initialising socket connection");
+    LOG_ERROR("Initialising socket connection");
     return -1;
   }
 
@@ -185,26 +184,26 @@ int main(int argc, char **argv) {
     broker_set_clientid(conn, clientid, sizeof(clientid));
   }
 
-  log_std(LOG_INFO, "Connecting to broker");
+  LOG_INFO("Connecting to broker");
 
   struct linux_broker_socket *skt = '\0';
   if ((ret = broker_connect(conn))) {
-    log_std(LOG_ERROR, "Initialising socket connection");
+    LOG_ERROR("Initialising socket connection");
     free_connection(conn);
     return ret;
   } else {
     skt = (struct linux_broker_socket *)conn->context;
-    log_std(LOG_INFO, "Connected to broker:\nip: %s port: %d", skt->ip, skt->port);
+    LOG_INFO("Connected to broker:\nip: %s port: %d", skt->ip, skt->port);
   }
 
-  log_std(LOG_INFO, "Subscribing to the following topics:");
-  log_std(LOG_INFO, "Topic: %s", topic);
+  LOG_INFO("Subscribing to the following topics:");
+  LOG_INFO("Topic: %s", topic);
 
   /* Find actual length of topic and subscribe */
   const char *end = strchr(topic, '\0');
   if (!end || (ret = broker_subscribe(conn, topic, end - topic))) {
 
-    log_std(LOG_ERROR, "Subscribing to topic.");
+    LOG_ERROR("Subscribing to topic.");
     ret = UMQTT_ERROR;
     goto free;
   }
@@ -212,7 +211,7 @@ int main(int argc, char **argv) {
   /* Start listening for packets */
   struct mqtt_packet *pkt = NULL;
   if (init_packet(&pkt)) {
-    log_std(LOG_ERROR, "Initialising packet");
+    LOG_ERROR("Initialising packet");
     ret = UMQTT_ERROR;
     goto free;
   }

src/inc/log.h
@@ -4,26 +4,58 @@
  * Author: Steven Swann - swannonline@googlemail.com
  *
  * Copyright (c) swannonline, 2013-2014
- * 
- * This file is part of sensorspace.
  *
- * sensorspace is free software: you can redistribute it and/or modify
+ * This file is part of uMQTT.
+ *
+ * uMQTT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * sensorspace is distributed in the hope that it will be useful,
+ * uMQTT is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with sensorspace.  If not, see <http://www.gnu.org/licenses/>.
+ * along with uMQTT.  If not, see <http://www.gnu.org/licenses/>.
  *
  *****************************************************************************/
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
+
+/*
+ * \brief Debug logs disabled by default
+ */
+#ifndef DEBUG
+#define DEBUG           0
+#define EN_DEBUG        0
+#else
+#define EN_DEBUG        DEBUG
+#endif
+
+/*
+ * \brief logs enabled by default
+ */
+#ifndef LOG_PRINTING
+#define EN_LOGGING      1
+#else
+#define EN_LOGGING      LOG_PRINTING
+#endif
+
+/*
+ * \brief Logging macros that allow printing to be removed
+ */
+#define LOG_ERROR(fmt, ...) \
+  do { if (EN_LOGGING) log_stderr(LOG_ERROR, fmt, ##__VA_ARGS__); } while (0)
+#define LOG_INFO(fmt, ...) \
+  do { if (EN_LOGGING) log_stdout(LOG_INFO, fmt, ##__VA_ARGS__); } while (0)
+
+#define LOG_DEBUG(fmt, ...) \
+  do { if (EN_DEBUG) log_stderr(LOG_DEBUG, fmt, ##__VA_ARGS__); } while (0)
+#define LOG_DEBUG_FN(fmt, ...) \
+  do { if (EN_DEBUG) log_stderr(LOG_DEBUG_FN, fmt, ##__VA_ARGS__); } while (0)
 
 typedef enum {
   LOG_NONE,

src/log.c
@@ -5,20 +5,20 @@
  *
  * Copyright (c) swannonline, 2013-2014
  *
- * This file is part of sensorspace.
+ * This file is part of uMQTT.
  *
- * sensorspace is free software: you can redistribute it and/or modify
+ * uMQTT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * sensorspace is distributed in the hope that it will be useful,
+ * uMQTT is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with sensorspace.  If not, see <http://www.gnu.org/licenses/>.
+ * along with uMQTT.  If not, see <http://www.gnu.org/licenses/>.
  *
  *****************************************************************************/
 #include "log.h"

commit 1ea4cd10613f63dd9d705e2454cc1aee0f63fee4

Fix bug in uMQTT_pub where pkts are repeated
Fix bug introduced in 7cad0bb3 whereby multiple publish packets are sent.
This was turned into a feature.

src/apps/uMQTT_pub.c
@@ -49,6 +49,7 @@ static int print_usage() {
       "Usage: uMQTT_pub [options] -m <PUBLISH message>\n"
       "General options:\n"
       " -h [--help]              : Displays this help and exits\n"
+      " -R [--repeat] <count>    : Send publish packet count times\n"
       "\n"
       "Publish options:\n"
       " -t [--topic] <topic>     : Change the default topic. Default: uMQTT_PUB\n"
@@ -72,6 +73,7 @@ static int print_usage() {
       "                                 INFO (default)\n"
       "                                 DEBUG\n"
       "                                 DEBUG_FN\n"
+      " -R [--repeat] <count>    : Send publish packet count times\n"
       "\n");
 
   return 0;
@@ -116,6 +118,7 @@ int main(int argc, char **argv) {
   int broker_port = MQTT_BROKER_PORT;
   char clientid[UMQTT_CLIENTID_MAX_LEN] = "\0";
   uint8_t retain = 0;
+  uint32_t repeat = 1;
 
   static struct option long_options[] =
   {
@@ -129,13 +132,14 @@ int main(int argc, char **argv) {
     {"broker", required_argument,       0, 'b'},
     {"port", required_argument,         0, 'p'},
     {"clientid", required_argument,     0, 'c'},
+    {"repeat", required_argument,       0, 'R'},
     {0, 0, 0, 0}
   };
 
   /* get arguments */
   while (1)
   {
-    if ((c = getopt_long(argc, argv, "hv:rt:m:b:p:c:f:", long_options,
+    if ((c = getopt_long(argc, argv, "hv:rt:m:b:p:c:f:R:", long_options,
             &option_index)) != -1) {
 
       switch (c) {
@@ -211,6 +215,13 @@ int main(int argc, char **argv) {
           } else {
             LOG_ERROR("The clientid flag should be followed by a clientid");
             return print_usage();
+          }
+          break;
+
+        case 'R':
+          /* set repeat count */
+          if (optarg) {
+            repeat = atoi(optarg);
           }
           break;
 
@@ -310,15 +321,15 @@ int main(int argc, char **argv) {
 
   LOG_INFO("Sending packet to broker");
 
-  int i =0;
-  while (i++ < 0xff) {
-  if ((ret = broker_send_packet(conn, pkt))) {
-    LOG_ERROR("Sending packet failed");
-
-  } else {
-    LOG_INFO("Successfully sent packet");
-  }
-  }
+  /* Send packets */
+  do {
+    ret = broker_send_packet(conn, pkt);
+    if (ret) {
+      LOG_ERROR("Sending packet failed");
+    } else {
+      LOG_INFO("Successfully sent packet");
+    }
+  } while (--repeat);
 
 free:
   LOG_INFO("Disconnecting from broker");